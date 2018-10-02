54b3a37d9b569cdbf053b35c895bd026c63e8416
bartvollmuller <bart.vollmuller@student.hu.nl>
2018-10-02 17:36:56 +0200

    Huiswerk

--- PROG_BartVollmuller_1738859_Huiswerk_1Formatting.py
+++ PROG_BartVollmuller_1738859_Huiswerk_1Formatting.py
@@ -1,1 +1,13 @@
 def convert(temperatuur):
+    temperatuurf = temperatuur*1.8 +32
+    return temperatuurf
+print(convert(25))
+
+def table():
+    print("\t""\t""F","\t""\t""\t""C")
+    for x in range(-30, 50, 10):
+        print("%10.2f %10.2f"  % (convert(x), x))
+#%10.2f heb ik opgezocht op Google i.v.m. niet weten hoe een mooie tab werkt.
+table()
+
+
