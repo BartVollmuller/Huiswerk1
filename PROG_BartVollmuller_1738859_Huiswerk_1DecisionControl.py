dfcea525225b031d78eece0062e2e03716165341
bartvollmuller <bart.vollmuller@student.hu.nl>
2018-10-02 10:37:03 +0200

    Huiswerk

---
+++ PROG_BartVollmuller_1738859_Huiswerk_1DecisionControl.py
@@ -0,0 +1,41 @@
+def seizoen(maand):
+    if maand == 3:
+        soortseizoen = "Dit is de lente"
+        return soortseizoen
+    if maand == 4:
+        soortseizoen = "Dit is de lente"
+        return soortseizoen
+    if maand == 5:
+        soortseizoen = "Dit is de lente"
+        return soortseizoen
+    if maand == 6:
+        soortseizoen = "Dit is de zomer"
+        return soortseizoen
+    if maand == 7:
+        soortseizoen = "Dit is de zomer"
+        return soortseizoen
+    if maand == 8:
+        soortseizoen = "Dit is de zomer"
+        return soortseizoen
+    if maand == 9:
+        soortseizoen = "Dit is de herfst"
+        return soortseizoen
+    if maand == 10:
+        soortseizoen = "Dit is de herfst"
+        return soortseizoen
+    if maand == 11:
+        soortseizoen = "Dit is de berfst"
+        return soortseizoen
+    if maand == 12:
+        soortseizoen = "Dit is de winter"
+        return soortseizoen
+    if maand == 1:
+        soortseizoen = "Dit is de winter"
+        return soortseizoen
+    if maand == 2:
+        soortseizoen = "Dit is de winter"
+        return soortseizoen
+
+gegeven = int(input("Wat is het nummer van de maand?"))
+print(seizoen(gegeven))
+
