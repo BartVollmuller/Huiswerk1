09fba7a6bd5cad202238c247504d5821d524587f
bartvollmuller <bart.vollmuller@student.hu.nl>
2018-10-02 17:38:03 +0200

    Huiswerk

---
+++ PROG_BartVollmuller_1738859_Huiswerk_2FilesLezen.py
@@ -0,0 +1,8 @@
+f = open('kaartnummers.txt','a')
+f.write("\nJan Jansen heeft kaartnummer : 325255\n"
+    "Erik Materus heeft kaartnummer: 334343\n"
+    "Ali Ahson heeft kaartnummer: 235434\n"
+    "Eva Versteeg heeft kaartnummer: 645345\n"
+    "Jan de Wilde heeft kaartnummer: 534545\n"
+    "Henk de Vrie heeft kaartnummer: 345355\n")
+f.close()
---
+++ PROG_BartVollmuller_1738859_Huiswerk_3Fileslezen.py
@@ -0,0 +1,8 @@
+file = open("kaartnummers.txt", 'r')
+num_lines = 0
+for line in file:
+        num_lines += 1
+print("Deze file telt " + str(num_lines) +" regels.")
+#Hoogste kaartnummer is niet mogelijk omdat ik de opdracht van het tekstbestand niet volledig juist heb gemaakt.
+#Hierdoor kan ik niet een max aanroepen van een bepaalde variabele
+
